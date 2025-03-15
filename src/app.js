81849b50f8e3bb8b10d69bf63058f3e504657607
climbingirl <isvetlanaivanova@yandex.ru>
2025-03-15 14:24:45 +0500

    inplement open/close menu logic and adding menu items

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
+import { ContextMenu } from './menu';
 import './styles.css';
+
+document.addEventListener('DOMContentLoaded', () => new ContextMenu('#menu'));
--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,36 @@
 import { Menu } from './core/menu';
+import { BackgroundModule } from './modules/background.module';
+import { ClicksModule } from './modules/clicks.module';
+import { ShapeModule } from './modules/shape.module';
+export class ContextMenu extends Menu {
+  menuItems = {
+    clicks: new ClicksModule('clicks', 'Считать клики (за 3 секундф)'),
+    shape: new ShapeModule('shape', 'Создать фигуру'),
+    background: new BackgroundModule('background', 'Поменять цвет'),
+  };
 
-export class ContextMenu extends Menu {}
+  constructor(element) {
+    super(element);
+    Object.values(this.menuItems).forEach((item) => this.add(item));
+    document.body.addEventListener('contextmenu', this.open.bind(this));
+    this.el.addEventListener('click', (event) => {
+      const type = event.target.dataset.type;
+      this.menuItems[type].trigger();
+    });
+  }
+
+  open(event) {
+    event.preventDefault();
+    this.el.style.top = `${event.offsetY}px`;
+    this.el.style.left = `${event.offsetX}px`;
+    this.el.classList.add('open');
+  }
+
+  close() {
+    this.el.classList.remove('open');
+  }
+
+  add(menuItem) {
+    this.el.innerHTML += menuItem.toHTML();
+  }
+}
