2669e7030185dbdd22da6152148057428890bc37
climbingirl <isvetlanaivanova@yandex.ru>
2025-03-16 20:05:43 +0500

    implement click analytis

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,3 +1,29 @@
 import { Module } from '../core/module';
+export class ClicksModule extends Module {
+  constructor(type, text) {
+    super(type, text);
+    this.durationSec = 3;
+    this.clickCount = 0;
+  }
 
-export class ClicksModule extends Module {}
+  trigger() {
+    this.start();
+  }
+
+  start() {
+    setTimeout(() => {
+      this.stop();
+    }, this.durationSec * 1000);
+    document.body.addEventListener('click', this.handleclick);
+  }
+
+  stop() {
+    document.removeEventListener('click', this.handleclick);
+    alert(`Total clicks: ${--this.clickCount}`);
+    this.clickCount = 0;
+  }
+
+  handleclick = () => {
+    this.clickCount++;
+  };
+}
